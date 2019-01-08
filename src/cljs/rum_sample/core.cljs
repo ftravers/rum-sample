a6ba97bfb32e3c609b819e49465de3c297352961
Fenton Travers <fenton.travers@gmail.com>
2019-01-07 20:15:45 -0800

    add test file and basic output for core file

--- src/cljs/rum_sample/core.cljs
+++ src/cljs/rum_sample/core.cljs
@@ -1,6 +1,6 @@
 (ns rum-sample.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(enable-console-print!)
+
+(println "Hello My Friend!")
+
---
+++ test/cljs/rum_sample/core_test.cljs
@@ -0,0 +1,7 @@
+(ns rum-sample.core-test
+  (:require [clojure.test :refer :all]
+            [rum-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
