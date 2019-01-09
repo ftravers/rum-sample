cb32587f3bf984b9e74463cfc7e5496da6703ffc
Fenton Travers <fenton.travers@gmail.com>
2019-01-09 14:04:13 -0800

    basic usage of just datascript

--- src/cljs/rum_sample/core.cljs
+++ src/cljs/rum_sample/core.cljs
@@ -1,13 +1,27 @@
 (ns rum-sample.core
-  (:require [rum.core :as rum]))
+  (:require
+   ;; [rum.core :as rum]
+   [datascript.core :as d]))
 
-(def text (atom "abc"))
+(def conn (d/create-conn))
 
-(rum/defc label < rum/reactive
-  [text]
-  [:div {:class "label"} (rum/react text)])
+(def datoms [{:db/id -1 :name "Bob" :age 30}
+             {:db/id -1 :name "Fenton" :age 45}
+             {:db/id -2 :name "Sally" :age 15}])
 
-(rum/mount (label text) js/document.body)
+(d/transact! conn datoms)
+
+(def data (d/q '[:find (pull ?e [:name :age])
+                 :where [?e :name "Fenton"]]
+               @conn))
+
+(js/console.log (str data))
+
+;; (rum/defc users < rum/reactive
+;;   [text]
+;;   [:div {:class "label"} text])
+
+;; (rum/mount (users "abc") js/document.body)
 
 
 
