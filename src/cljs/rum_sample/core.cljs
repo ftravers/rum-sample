38797cdbbf20971de3c059ea7f5e6acddaf9a740
Fenton Travers <fenton.travers@gmail.com>
2019-01-09 09:52:12 -0800

    basic Garden CSS added

--- src/cljs/rum_sample/core.cljs
+++ src/cljs/rum_sample/core.cljs
@@ -1,8 +1,13 @@
 (ns rum-sample.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [garden.core :refer [css]]))
 
-(enable-console-print!)
+(defn my-comp []
+  [:div 
+   [:style (css [:.mine {:font-size "10px"}])]
+   [:div {:class "mine"} "Hello World!!!"]])
 
-(r/render [:p "Hello World!"] (js/document.getElementById "app"))
+(r/render (my-comp) 
+          (js/document.getElementById "app"))
 
 
