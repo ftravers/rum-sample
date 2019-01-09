f69c9ee5d69302151b7cf04a9643ccaab6ba8ca1
Fenton Travers <fenton.travers@gmail.com>
2019-01-09 11:41:25 -0800

    basic CSS Grid fractional columns

--- src/cljs/rum_sample/core.cljs
+++ src/cljs/rum_sample/core.cljs
@@ -2,10 +2,23 @@
   (:require [reagent.core :as r]
             [garden.core :refer [css]]))
 
+(def css-data
+  (css
+   [[:.grid
+     {:display :grid
+      :grid-template-columns "1fr 1fr"
+      :grid-gap "20px"}]
+    
+    [:.grid :div
+     {:border "1px solid rgb(0,95,100)"}]]))
+
 (defn my-comp []
-  [:div 
-   [:style (css [:.mine {:font-size "10px"}])]
-   [:div {:class "mine"} "Hello World!!!"]])
+  [:div {:class "grid"}
+   [:style css-data]
+   [:div "1"]
+   [:div "2"]
+   [:div "3"]
+   [:div "4"]])
 
 (r/render (my-comp) 
           (js/document.getElementById "app"))
